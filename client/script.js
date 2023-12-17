1dd078da251962fa89e1a8f8e9272f065c318478
Anuoluwapo Gabriel Oyetibo <54692909+Aoyetibo557@users.noreply.github.com>
2023-12-17 17:12:33 -0500

    Initial commit

---
+++ client/script.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+    // I want to get the form element
+    const form = $('#contact-form');
+    const success = $('#success');
+    const errorMessage = $('#msg');
+    let isAjaxSuccess = false;
+
+    success.hide();
+
+    // Add an event listener to the form
+    form.on('submit', function(e) {
+        // Prevent the form from submitting
+        e.preventDefault();
+
+        // Validate the form
+        validateForm();
+
+        // Get the form data at the time of submission
+        const formData = form.serialize();
+
+        // Set the URL for the AJAX request
+        const formAction = 'http://localhost:8080/api/contact';
+
+        // Send the form data to the server
+        $.ajax({
+            url: formAction,
+            method: 'POST',
+            data: formData,
+            success: function(data, textStatus, xhr) {
+                if (xhr.status === 200) {
+                    isAjaxSuccess = true;
+                    errorMessage.text('');
+
+
+                    // Clear the form fields upon successful submission
+                    form.trigger('reset');
+
+                    // Hide the form and display the success message
+                    form.hide();
+                    success.show();
+
+                } else if (xhr.status === 400) {
+                    errorMessage.text(data.msg);
+                }
+            },
+            error: function(xhr, textStatus, err) {
+                errorMessage.text('Something went wrong. Please try again later.');
+            }
+        });
+
+    });
+
+    // $('#name, #email, #message').on('input', function() {
+    //     if (isAjaxSuccess) {
+    //         errorMessage.text('');
+    //         isAjaxSuccess = false;
+    //     }
+    // });
+
+});
+
+// function to validate form
+function validateForm() {
+    // get form values
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // check if form values are empty
+    if (!name || !email || !message) {
+        $('#msg').text('Please fill out all missing fields');
+        return false;
+    }
+
+    // check if email is valid
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        $('#msg').text('Please enter a valid email address');
+        return false;
+    }
+
+}
---
+++ server/index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
+const { open } = require('sqlite')
+
+const app = express();
+const port = 8080 || process.env.PORT;
+
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// open sqlite database connection
+const dbPromise = open({
+    filename: './mycontacts.db',
+    driver: sqlite3.Database
+});
+
+/*
+Create the 'contacts' table */
+// async function createTable() {
+//     const db = await dbPromise;
+//     await db.exec(`
+//         CREATE TABLE IF NOT EXISTS contacts (
+//             id INTEGER PRIMARY KEY,
+//             name TEXT,
+//             email TEXT,
+//             message TEXT
+//         )
+//     `);
+// }
+
+// createTable();
+
+// middleware that lifts the cors restrictions for routing from a different url (localhost:3000)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+//middleware that logs the request method and url
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+/* Routes */
+app.get('/', (req, res) => {
+    res.send('Hello User Welcome to the TravelMate Task API!');
+});
+
+app.get('/api', (req, res) => {
+    res.send(`API is running!, Please use the following endpoint: /api/contact - POST, /api/view-contacts - GET`);
+});
+
+// post endpoint to collect data (name, email, message) from contact form
+app.post('/api/contact', async(req, res) => {
+    const { name, email, message } = req.body;
+    if (!name || !email || !message) {
+        return res.status(400).json({ status: 400, msg: 'Please enter all fields' });
+    }
+
+    try {
+        const db = await dbPromise;
+        await db.run('INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)', [name, email, message]);
+
+        return res.status(200).json({ status: 200, msg: 'Message sent successfully' });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ status: 500, msg: 'Something went wrong. Please try again later.' });
+
+    }
+    // finally {
+    //     await db.close();
+    // }
+});
+
+
+// view all contacts
+app.get('/api/view-contacts', async(req, res) => {
+    try {
+        const db = await dbPromise;
+        const contacts = await db.all('SELECT * FROM contacts');
+
+        return res.status(200).json({ status: 200, data: contacts });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ status: 500, msg: 'Something went wrong. Please try again later.' });
+
+    }
+    // finally {
+    //     await db.close();
+    // }
+});
+
+/* Start server */
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
